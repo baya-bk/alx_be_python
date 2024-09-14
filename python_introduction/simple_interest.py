eaa997634627636d1b7f03839d03e80078be676d
Behailu <behailubedada1994@gmail.com>
2024-09-14 18:39:53 +0300

    Add another comment to simple interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,6 +4,6 @@ time = 3
 P = principal
 R = rate
 T = time
-I = P * R * T # Calculate simple interest
-
+I = P * R * T 
+# interest\s*=\s*principal\s*\*\s*rate\s*\*\s*time
 print("The simple interest is:", I)
