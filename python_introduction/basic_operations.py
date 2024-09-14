bb3faf8dc4932fd2bb91c832cdd6e8dbc617f996
Behailu <behailubedada1994@gmail.com>
2024-09-14 17:32:53 +0300

    Fix variable assignment error for number2

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 number1 = int(10)
 number2 = int(5)
-sum = number1 + number1 
+sum = number1 + number2 
 sub = number1 - number2 
 print("sum of", number1, "and", number2, "is", sum) 
 print("subtraction of", number1, "and", number2, "is", sub)
