680e4f9d6f25df5da1a74cb815f3dd7082a5c1f9
Behailu <behailubedada1994@gmail.com>
2024-09-14 17:36:12 +0300

    Add multiplication operation

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,16 @@
 number1 = int(10)
 number2 = int(5)
-sum = number1 + number2 
-sub = number1 - number2 
-print("sum of", number1, "and", number2, "is", sum) 
+
+# Addition
+sum = number1 + number2
+
+# Subtraction
+sub = number1 - number2
+
+# Multiplication
+product = number1 * number2  # Add this line for multiplication
+
+# Print the results
+print("sum of", number1, "and", number2, "is", sum)
 print("subtraction of", number1, "and", number2, "is", sub)
+print("multiplication of", number1, "and", number2, "is", product)
