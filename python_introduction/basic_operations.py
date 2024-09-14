9b66b9c1723c5c619f148bf8638259c20951197c
Behailu <behailubedada1994@gmail.com>
2024-09-14 16:59:27 +0300

    Write basic operations in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number2 = int(10)
+number1 = int(5)
+sum = number2 + number1 
+sub = number2 - number1 
+print("sum of", number2, "and", number1, "is", sum) 
+print("subtraction of", number2, "and", number1, "is", sub)
