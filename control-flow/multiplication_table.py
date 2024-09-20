27e71fa46dc4af775865252d9fcafb9be567a848
Behailu <behailubedada1994@gmail.com>
2024-09-20 13:33:26 +0300

    Made Simple for loop that builds mult. table from user's input

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
+for num in range(1, 11):
+    result = number * num
+    print(f"{number} * {num} = {result}")
+
+# recieve a number from user
+# store in var named number
+# write basic for loop structure that executes a statement in 1-10 range
+#  multiply user input with num and store it in result variable
+# print the time table using print
