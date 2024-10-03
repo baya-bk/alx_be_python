616c7c2955dd2f2fb047ada0ac692715ef0fc42d
Behailu <behailubedada1994@gmail.com>
2024-10-03 18:36:23 +0300

    Add elif to zero division handling

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -14,8 +14,9 @@ def perform_operation(num1, num2, operation):
 
         case "divide":
             if num2 == 0:
-                return "Cannot divide by zero."
-            else:
+                return "Cannot divide by zero."  # Handle division by zero
+            elif num2 != 0:
                 result = num1 / num2
-                # print("The result is: ", result)
+        case _:
+            return "Invalid operation"  # Handle invalid operations
     return result
